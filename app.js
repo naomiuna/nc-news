5657341a767c0a7988fb9b2263f1827d81d2cb56
Naomi Todd <naomiunatodd@gmail.com>
2020-03-23 17:00:58 +0000

    set up error file and added 500 server error

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/apiRouter');
+const {handle500s} = require('./errors')
 app.use(express.json());
 
 app.use('/api', apiRouter);
+
+app.use(handle500s);
 
 module.exports = app;
