694be904ccfc98d6d7107f4d1a0b1fca0867daa6
Naomi Todd <naomiunatodd@gmail.com>
2020-03-23 16:00:31 +0000

    tested util functions formatDates, makeRefObj and formatComments

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,29 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  return list.map(item => {
+  return {...item, created_at: new Date(item.created_at)}
+  })
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = (list, key, val) => {
+  const result = {};
+  list.forEach(item => {
+    result[item[key]] = item[val];
+  })
+  return result;
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  // if (comments.length === 0) return [];
+  // const { belongs_to, created_by, ...rest } = comments[0];
+  // result = [{...rest}];
+  // result[0].author = created_by;
+  // result[0].article_id = articleRef[belongs_to]
+  // return result;
+  return comments.map(({ belongs_to, created_by, ...rest }) => {
+    return {
+      author: created_by,
+      article_id: articleRef[belongs_to],
+      ...rest
+    }
+  })
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,8 +5,352 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('returns a new empty array when passed an empty array', () => {
+    const input = [];
+    const actual = formatDates(input)
+    
+    expect(actual).to.eql([]);
+    expect(actual).not.to.equal(input);
+  })
+  it('returns an array with timestamp updated for one object', () => {
+    const input = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = formatDates(input);
+    const expected = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: new Date(1542284514171),
+        votes: 100
+      }
+    ];
+    expect(actual).to.eql(expected);
+  })
+  it('returns an array with timestamp updated for multiple objects', () => {
+    const input = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: 1416140514171
+      },
+      {
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: 1289996514171
+      }
+    ];
+    const acutal = formatDates(input);
+    const expected = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: new Date(1542284514171),
+        votes: 100
+      },
+      {
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: new Date (1416140514171)
+      },
+      {
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: new Date (1289996514171)
+      }
+    ];
+    expect(acutal).to.eql(expected);
+  })
+  it('does not mutate original array', () => {
+    const input = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: 1416140514171
+      },
+      {
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: 1289996514171
+      }
+    ];
+    formatDates(input);
+    expect(input).to.eql([
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: 1416140514171
+      },
+      {
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: 1289996514171
+      }
+    ]);
+  })
+});
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => {
+  it('returns an empty object when passed an empty array', () => {
+    expect(makeRefObj([])).to.eql({});
+  })
+  it('returns a reference object from an array with one element', () => {
+    const input = [
+      {
+        article_id: 1,
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = makeRefObj(input, 'title', 'article_id');
+    const expected = { 'Living in the shadow of a great man': 1 }
+    expect(actual).to.eql(expected);
+  })
+  it('returns a reference object from an array with multiple elements', () => {
+    const input = [
+      {article_id:1,
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {article_id: 2,
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: 1416140514171
+      },
+      {article_id: 3,
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: 1289996514171
+      }
+    ];
+    const actual = makeRefObj(input, 'title', 'article_id');
+    const expected = {
+      'Living in the shadow of a great man': 1,
+      'Sony Vaio; or, The Laptop': 2,
+      'Eight pug gifs that remind me of mitch': 3
+    }
+    expect(actual).to.eql(expected);
+  })
+});
 
-describe('formatComments', () => {});
+describe.only('formatComments', () => {
+  it('returns a new empty array when passed an empty array', () => {
+      const input = [];
+      const actual = formatComments(input);
+
+      expect(actual).to.eql([]);
+      expect(actual).not.to.equal(input);
+  })
+  it('returns updated array with item updated when passed a single item array', () => {
+    const input = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: new Date (1511354163389)
+      }
+    ];
+    const ref = { "They're not exactly dogs, are they?": 9 }
+    const actual = formatComments(input, ref);
+    const expected = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        article_id: 9,
+        author: 'butter_bridge',
+        votes: 16,
+        created_at: new Date (1511354163389)
+      }
+    ];
+    expect(actual).to.eql(expected);
+  })
+  it('returns updated array with item updated when passed a single item array', () => {
+    const input = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: 1511354163389
+      },
+      {
+        body:
+          'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
+        votes: 14,
+        created_at: 1479818163389
+      },
+      {
+        body:
+          'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'icellusedkars',
+        votes: 100,
+        created_at: 1448282163389
+      }
+    ];
+    const ref = { "They're not exactly dogs, are they?": 9, 'Living in the shadow of a great man': 1 };
+    const actual = formatComments(input, ref);
+    const expected = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        article_id: 9,
+        author: 'butter_bridge',
+        votes: 16,
+        created_at: 1511354163389
+      },
+      {
+        body:
+          'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+        article_id: 1,
+        author: 'butter_bridge',
+        votes: 14,
+        created_at: 1479818163389
+      },
+      {
+        body:
+          'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+        article_id: 1,
+        author: 'icellusedkars',
+        votes: 100,
+        created_at: 1448282163389
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
+  it('does not mutate original array or reference data', () => {
+const input = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: 1511354163389
+      },
+      {
+        body:
+          'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
+        votes: 14,
+        created_at: 1479818163389
+      },
+      {
+        body:
+          'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'icellusedkars',
+        votes: 100,
+        created_at: 1448282163389
+      }
+    ];
+    const ref = { "They're not exactly dogs, are they?": 9, 'Living in the shadow of a great man': 1 };
+    formatComments(input, ref);
+    expect(input).to.eql([
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: 1511354163389
+      },
+      {
+        body:
+          'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'butter_bridge',
+        votes: 14,
+        created_at: 1479818163389
+      },
+      {
+        body:
+          'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+        belongs_to: 'Living in the shadow of a great man',
+        created_by: 'icellusedkars',
+        votes: 100,
+        created_at: 1448282163389
+      }
+    ]);
+    expect(ref).to.eql({
+      "They're not exactly dogs, are they?": 9,
+      'Living in the shadow of a great man': 1
+    });
+
+  })
+});
