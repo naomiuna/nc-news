a0934e1c3f28ca6a087e934ee176a1aa9573bf32
Naomi Todd <naomiunatodd@gmail.com>
2020-03-24 14:47:32 +0000

    deleted console.log()

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,7 +4,6 @@ exports.getUserById = (req, res, next) => {
   const { username } = req.params;
   fetchUserById(username)
     .then(user => {
-      console.log(user);
       res.status(200).send({user})
     })
     .catch(next)
