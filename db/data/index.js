b0b89b4769b5f66388fc0649022e5d639c7bf49a
Naomi Todd <naomiunatodd@gmail.com>
2020-03-26 16:08:08 +0000

    setup heroku hosting

--- db/data/index.js
+++ db/data/index.js
@@ -3,6 +3,6 @@ const ENV = process.env.NODE_ENV || 'development';
 const development = require('./development-data');
 const test = require('./test-data');
 
-const data = { development, test };
+const data = { development, test, production: development };
 
 module.exports = data[ENV];
